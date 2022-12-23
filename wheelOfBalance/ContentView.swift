7dd4de692b2fd3ad66b078b95fc22a2dbf988e30
Oleksii Mykhalchuk <ale.m100888@gmail.com>
2022-12-23 18:31:32 +0100

    added labels

--- wheelOfBalance/ContentView.swift
+++ wheelOfBalance/ContentView.swift
@@ -10,24 +10,50 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            ZStack {
-                DrawShapes()
-                CircleShape(width: 300)
-                DrawLines()
-                DrawWheel(
-                    width: 150,
-                    career: 9,
-                    spiritual: 8,
-                    environment: 7,
-                    social: 6,
-                    family: 5,
-                    health: 4,
-                    growth: 3,
-                    finance: 2)
-                LabelsView()
+            VStack {
+
+                wheelView
+
+                entryView
+                Spacer()
             }
             .navigationTitle("Wheel of Balance")
             .background(.mint)
+        }
+    }
+
+    var wheelView: some View {
+        ZStack {
+            DrawShapes()
+            CircleShape(width: 300)
+            DrawLines()
+            DrawWheel(
+                width: 150,
+                career: 9,
+                spiritual: 8,
+                environment: 7,
+                social: 6,
+                family: 5,
+                health: 4,
+                growth: 3,
+                finance: 2)
+            LabelsView()
+        }
+    }
+    @State var input: String = ""
+    var entryView: some View {
+        VStack {
+            Text("Career: ")
+            TextField(text: $input) {
+                Text("Career")
+            }
+            Text("Spiritual: ")
+            Text("Environment: ")
+            Text("Social: ")
+            Text("Family: ")
+            Text("Health: ")
+            Text("Growth: ")
+            Text("Finance: ")
         }
     }
 }
