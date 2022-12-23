d72c77c7b2cec6230b611136e4ac23ede231e29a
Oleksii Mykhalchuk <ale.m100888@gmail.com>
2022-12-23 15:28:24 +0100

    Initial Commit

---
+++ wheelOfBalance/wheelOfBalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  wheelOfBalanceApp.swift
+//  wheelOfBalance
+//
+//  Created by Oleksii Mykhalchuk on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct wheelOfBalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
